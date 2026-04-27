ed8b944d01ad217eaba2d9ac8b495fd31981b3bc
Palawi <bapcoloj@gmail.com>
2026-04-27 15:12:05 +0200

    feat(booster): add booster model

---
+++ src/main/java/domain/booster/Booster.java
@@ -0,0 +1,20 @@
+package domain.booster;
+
+public class Booster {
+    private final String name;
+    private final double additionalThrustKilonewtons;
+    private final double massTons;
+    private final double priceMillionEuros;
+
+    public Booster(String name, double additionalThrustKilonewtons, double massTons, double priceMillionEuros) {
+        this.name = name;
+        this.additionalThrustKilonewtons = additionalThrustKilonewtons;
+        this.massTons = massTons;
+        this.priceMillionEuros = priceMillionEuros;
+    }
+
+    public String getName() {return name;}
+    public double getAdditionalThrustKilonewtons() {return additionalThrustKilonewtons;}
+    public double getMassTons() {return massTons;}
+    public double getPriceMillionEuros() {return priceMillionEuros;}
+}
