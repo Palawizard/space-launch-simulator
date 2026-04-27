d6aea13a2db19dc66915af63d3353f26df391324
Palawi <bapcoloj@gmail.com>
2026-04-27 16:03:22 +0200

    feat(rocket): add rocket composition

---
+++ src/main/java/domain/rocket/Rocket.java
@@ -0,0 +1,56 @@
+package domain.rocket;
+
+import domain.booster.Booster;
+import domain.capsule.Capsule;
+import domain.launcher.Launcher;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Rocket {
+    private final Launcher launcher;
+    private final Capsule capsule;
+    private final List<Booster> boosters;
+
+    public Rocket(Launcher launcher, Capsule capsule, List<Booster> boosters) {
+        this.launcher = launcher;
+        this.capsule = capsule;
+        this.boosters = new ArrayList<>(boosters);
+    }
+
+    public Launcher getLauncher() {
+        return launcher;
+    }
+
+    public Capsule getCapsule() {
+        return capsule;
+    }
+
+    public List<Booster> getBoosters() {
+        return Collections.unmodifiableList(boosters);
+    }
+
+    public double getTotalMassTons() {
+        double total = capsule.getMassTons();
+        for (Booster booster : boosters) {
+            total += booster.getMassTons();
+        }
+        return total;
+    }
+
+    public double getTotalPriceMillionEuros() {
+        double total = launcher.getPriceMillionEuros() + capsule.getPriceMillionEuros();
+        for (Booster booster : boosters) {
+            total += booster.getPriceMillionEuros();
+        }
+        return total;
+    }
+
+    public String getSummary() {
+        return "Launcher: " + launcher.getName()
+                + "\nCapsule: " + capsule.getName()
+                + "\nBoosters: " + boosters.size()
+                + "\nTotal mass: " + getTotalMassTons() + " t"
+                + "\nTotal price: " + getTotalPriceMillionEuros() + " M EUR";
+    }
+}
