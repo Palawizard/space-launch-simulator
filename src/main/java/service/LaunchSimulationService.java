62584cd130e093cf05fcecef1984209b42ad9a8f
Palawi <bapcoloj@gmail.com>
2026-04-28 14:07:33 +0200

    feat(simulation): calculate launch cost

--- src/main/java/service/LaunchSimulationService.java
+++ src/main/java/service/LaunchSimulationService.java
@@ -4,7 +4,15 @@ import domain.mission.Mission;
 import domain.rocket.Rocket;
 
 public class LaunchSimulationService {
+    public static final double PRICE_KEROSENE_PER_TON = 1200;
+
     public double calculateFuelRequiredTons(Rocket rocket, Mission mission) {
         return (rocket.getTotalMassTons() * mission.getDistanceKilometers() * mission.getFuelCoefficient()) / 1000;
+    }
+
+    public double calculateLaunchCostEuros(Rocket rocket, Mission mission) {
+        double rocketPriceEuros = rocket.getTotalPriceMillionEuros() * 1_000_000;
+        double fuelPriceEuros = calculateFuelRequiredTons(rocket, mission) * PRICE_KEROSENE_PER_TON;
+        return rocketPriceEuros + fuelPriceEuros;
     }
 }
