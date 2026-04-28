c2d8190307c6c64a8e3241caddaef871345479c0
Palawi <bapcoloj@gmail.com>
2026-04-28 14:18:05 +0200

    feat(simulation): validate launch conditions

--- src/main/java/service/LaunchSimulationService.java
+++ src/main/java/service/LaunchSimulationService.java
@@ -2,6 +2,11 @@ package service;
 
 import domain.mission.Mission;
 import domain.rocket.Rocket;
+import exception.IncompatibleCrewedMissionException;
+import exception.InsufficientFuelException;
+import exception.LaunchException;
+import exception.PayloadCapacityExceededException;
+import exception.TooManyBoostersException;
 
 public class LaunchSimulationService {
     public static final double PRICE_KEROSENE_PER_TON = 1200;
@@ -14,5 +19,23 @@ public class LaunchSimulationService {
         double rocketPriceEuros = rocket.getTotalPriceMillionEuros() * 1_000_000;
         double fuelPriceEuros = calculateFuelRequiredTons(rocket, mission) * PRICE_KEROSENE_PER_TON;
         return rocketPriceEuros + fuelPriceEuros;
+    }
+
+    public void validateLaunchConditions(Rocket rocket, Mission mission) throws LaunchException {
+        if (calculateFuelRequiredTons(rocket, mission) > rocket.getLauncher().getMaxFuelTons()) {
+            throw new InsufficientFuelException();
+        }
+
+        if (rocket.getTotalMassTons() > rocket.getLauncher().getPayloadCapacityTons()) {
+            throw new PayloadCapacityExceededException();
+        }
+
+        if (rocket.getBoosters().size() > rocket.getLauncher().getMaxBoosters()) {
+            throw new TooManyBoostersException();
+        }
+
+        if (mission.isCrewRequired() && (!rocket.getCapsule().isCrewed() || rocket.getCapsule().getMaxOccupants() <= 0)) {
+            throw new IncompatibleCrewedMissionException();
+        }
     }
 }
