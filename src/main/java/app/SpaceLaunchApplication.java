b91919af610638455ebd1760969ea8eb27c0d18b
Palawi <bapcoloj@gmail.com>
2026-04-30 14:46:58 +0200

    feat(history): load history on startup

--- src/main/java/app/SpaceLaunchApplication.java
+++ src/main/java/app/SpaceLaunchApplication.java
@@ -1,7 +1,29 @@
 package app;
 
+import java.io.IOException;
+import persistence.LaunchHistoryService;
+
 public class SpaceLaunchApplication {
+    private final LaunchHistoryService launchHistoryService;
+
+    public SpaceLaunchApplication() {
+        this(new LaunchHistoryService());
+    }
+
+    public SpaceLaunchApplication(LaunchHistoryService launchHistoryService) {
+        this.launchHistoryService = launchHistoryService;
+    }
+
     public void start() {
+        loadHistory();
         System.out.println("Space Launch Simulator is ready.");
+    }
+
+    private void loadHistory() {
+        try {
+            launchHistoryService.loadHistory();
+        } catch (IOException exception) {
+            throw new IllegalStateException("Unable to load launch history", exception);
+        }
     }
 }
